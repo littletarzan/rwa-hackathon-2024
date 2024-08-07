38c9a280582956b5c9cf208d536c1bf6dd240767
littletarzan <mdelore.ufl@gmail.com>
2024-08-07 11:35:09 -0400

    rm unused safecast functions

--- contracts/libraries/SafeCast.sol
+++ contracts/libraries/SafeCast.sol
@@ -2,14 +2,6 @@
 pragma solidity ^0.8.20;
 
 library SafeCast {
-    function toInt256(uint256 value) internal pure returns (int256) {
-        // Note: Unsafe cast below is okay because `type(int256).max` is guaranteed to be positive
-        require(
-            value <= uint256(type(int256).max),
-            "SafeCast: value doesn't fit in an int256"
-        );
-        return int256(value);
-    }
 
     function toInt64(int256 value) internal pure returns (int64) {
         require(
@@ -34,9 +26,4 @@ library SafeCast {
         );
         return int64(uint64(value));
     }
-
-    function toUint64(uint256 value) internal pure returns (uint64) {
-        require(value <= type(uint64).max, "SafeCast: value doesn't fit in 64 bits");
-        return uint64(value);
-    }    
 }
