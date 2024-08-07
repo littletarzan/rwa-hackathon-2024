b25447aafc1a9c461c5a54f083e8599ba8ed55d0
littletarzan <mdelore.ufl@gmail.com>
2024-08-07 14:02:30 -0400

    add testnet support

--- EnvContainer.ts
+++ EnvContainer.ts
@@ -34,11 +34,10 @@ export default class EnvContainer {
     HTS1400Contract?: ContractId
     HTS1400Token?: TokenId
 
-    // fill in other cases if network is not local
-    constructor(network: string = hardhat.network.name, path: string="./.env") {
-        // switch(network) {
-            // case 'local':
-            
+    constructor(network: string = hardhat.network.name) {
+        switch(network) {
+            case 'local':
+                default:
                 this.myAccountId = AccountId.fromString(process.env.LOCAL_MYACCOUNTID!);
                 this.myPrivateKey = PrivateKey.fromStringECDSA(process.env.LOCAL_MYPRIVATEKEY!);
                 this.myClient = Client.forLocalNode().setOperator(this.myAccountId, this.myPrivateKey);
@@ -69,24 +68,41 @@ export default class EnvContainer {
 
                 this.HTS1400Contract = ContractId.fromString(process.env.LOCAL_HTS1400_CONTRACT_ID!);
                 this.HTS1400Token = TokenId.fromString(process.env.LOCAL_HTS1400_TOKEN_ID!)
-            //     break;            
-            // case 'previewnet': // add the rest to previewnet and local if using those
+            break;
+            case 'testnet':
+                this.myAccountId = AccountId.fromString(process.env.TESTNET_MYACCOUNTID!);
+                this.myPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_MYPRIVATEKEY!);
+                this.myClient = Client.forTestnet().setOperator(this.myAccountId, this.myPrivateKey);
 
+                this.aliceId = AccountId.fromString(process.env.TESTNET_ALICE_ID!);
+                this.alicePrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_ALICE_PRIVATEKEY!);
+                this.aliceClient = Client.forTestnet().setOperator(this.aliceId, this.alicePrivateKey);
             
-            //     break;
-            // case 'testnet':
-            // default:
-                
-            //     this.myAccountId = AccountId.fromString(process.env.TESTNET_MYACCOUNTID!);
-            //     this.myPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_MYPRIVATEKEY!);
-            //     this.myClient = Client.forTestnet().setOperator(this.myAccountId, this.myPrivateKey);
+                this.bobId = AccountId.fromString(process.env.TESTNET_BOB_ID!);
+                this.bobPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_BOB_PRIVATEKEY!);
+                this.bobClient = Client.forTestnet().setOperator(this.bobId, this.bobPrivateKey);
 
-            //     this.aliceId = AccountId.fromString(process.env.TESTNET_ALICE_ID!);
-            //     this.alicePrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_ALICE_PRIVATEKEY!);
-            //     this.aliceClient = Client.forTestnet().setOperator(this.aliceId, this.alicePrivateKey);
+                this.charlieId = AccountId.fromString(process.env.TESTNET_CHARLIE_ID!);
+                this.charliePrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_CHARLIE_PRIVATEKEY!);
+                this.charlieClient = Client.forTestnet().setOperator(this.charlieId, this.charliePrivateKey);
 
-            //     break;
-        // }
+                this.operatorId = AccountId.fromString(process.env.TESTNET_OPERATOR_ID!);
+                this.operatorPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_OPERATOR_PRIVATEKEY!);
+                this.operatorClient = Client.forTestnet().setOperator(this.operatorId, this.operatorPrivateKey);
+
+                this.controllerId = AccountId.fromString(process.env.TESTNET_CONTROLLER_ID!);
+                this.controllerPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_CONTROLLER_PRIVATEKEY!);
+                this.controllerClient = Client.forTestnet().setOperator(this.controllerId, this.controllerPrivateKey);
+
+                this.ownerId = AccountId.fromString(process.env.TESTNET_OWNER_ID!);
+                this.ownerPrivateKey = PrivateKey.fromStringECDSA(process.env.TESTNET_OWNER_PRIVATEKEY!);
+                this.ownerClient = Client.forTestnet().setOperator(this.ownerId, this.ownerPrivateKey);
+
+                this.HTS1400Contract = ContractId.fromString(process.env.TESTNET_HTS1400_CONTRACT_ID!);
+                this.HTS1400Token = TokenId.fromString(process.env.TESTNET_HTS1400_TOKEN_ID!)
+
+            break;
+        }
 
     }
 }
